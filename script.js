9c7fffeead89cb0e8f8050d50182f674ba140c72
Alexander Hills <alexanderhills@gmail.com>
2019-06-02 21:32:59 +0100

    Adjusted gutter width

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 $(document).on('click', '[data-toggle="lightbox"]', function(event) {
     event.preventDefault();
-    $(this).ekkoLightbox();
+    $(this).ekkoLightbox({ 
+        alwaysShowClose: true
+
+    });
 });
 
